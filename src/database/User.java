cb928b0af12ed4f0dceb2f6071e9dacc74832e4a
Kenneth Korcal <kakorcal@gmail.com>
2017-12-10 11:05:49 -0800

    temp commit

--- src/database/User.java
+++ src/database/User.java
@@ -1,10 +1,22 @@
 package database;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "User")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // specifies that id is a primary key
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    public User() {}
 
 }

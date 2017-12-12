abbaba49f5b54baa2e9430aeda4095e79be5f594
Kenneth Korcal <kakorcal@gmail.com>
2017-12-12 09:12:44 -0800

    adding error validations on inventory

--- src/database/InventoryTransaction.java
+++ src/database/InventoryTransaction.java
@@ -11,29 +11,62 @@ import java.util.List;
 public class InventoryTransaction {
     private SessionFactory sessionFactory;
     private User user;
+    private ErrorHandler error;
 
     public InventoryTransaction(SessionManager manager, User user) {
         this.user = user;
         this.sessionFactory = manager.getSessionFactory();
+        this.error = new ErrorHandler();
     }
 
-    public Inventory create(Inventory inventory) throws HibernateException {
+    public Inventory create(Inventory inventory) {
         Session session = sessionFactory.openSession();
         Transaction transaction = null;
+        List<Inventory> list;
+        error.reset();
+
+        // input validations
+        if(user == null) {
+            error.setMessage("Unauthorized to create inventory.");
+            return null;
+        }
+
+        if(inventory.getName().isEmpty()) {
+            error.setMessage("Please enter an inventory name.");
+            return null;
+        }
+
+        if(inventory.getQuantity() < 0) {
+            error.setMessage("Quantity must be larger than 0.");
+            return null;
+        }
 
         try {
             transaction = session.beginTransaction();
-            inventory.setUser(user);
-            session.save(inventory);
-            transaction.commit();
-            System.out.println("Inventory created: ");
-            System.out.println(inventory.toString());
+
+            // query to check if duplicate name exists
+            Query<Inventory> query = session.createQuery("FROM Inventory I WHERE I.user.id = :user_id AND I.name = :inventory_name", Inventory.class);
+            query.setParameter("user_id", user.getId());
+            query.setParameter("inventory_name", inventory.getName());
+            list = query.list();
+
+            if(!list.isEmpty()) {
+                error.setMessage("Inventory name must be unique.");
+                return null;
+            }else {
+                inventory.setUser(user);
+                session.save(inventory);
+                transaction.commit();
+                System.out.println("Inventory created: ");
+                System.out.println(inventory.toString());
+            }
         }catch (HibernateException e) {
             if(transaction != null) {
                 transaction.rollback();
             }
             e.printStackTrace();
-            throw new HibernateException(e);
+            error.setMessage("Failed to create inventory.");
+            return null;
         }finally {
             session.close();
         }
@@ -41,10 +74,21 @@ public class InventoryTransaction {
         return inventory;
     }
 
-    public Inventory read(long id) throws HibernateException {
+    public Inventory read(long id) {
         Session session = sessionFactory.openSession();
         Transaction transaction = null;
         Inventory inventory;
+        error.reset();
+
+        if(user == null) {
+            error.setMessage("Unauthorized to read inventory.");
+            return null;
+        }
+
+        if(id < 0) {
+            error.setMessage("Invalid inventory identifier.");
+            return null;
+        }
 
         try {
             transaction = session.beginTransaction();
@@ -56,7 +100,8 @@ public class InventoryTransaction {
                 System.out.println(inventory.toString());
             }else {
                 System.out.println("Inventory id: " + id + " does not exist");
-                inventory = null;
+                error.setMessage("Specified inventory does not exist.");
+                return null;
             }
 
             transaction.commit();
@@ -65,7 +110,8 @@ public class InventoryTransaction {
                 transaction.rollback();
             }
             e.printStackTrace();
-            throw new HibernateException(e);
+            error.setMessage("Failed to retrieve inventory data.");
+            return null;
         }finally {
             session.close();
         }
@@ -73,17 +119,40 @@ public class InventoryTransaction {
         return inventory;
     }
 
-    public Inventory update(Inventory inventory) throws HibernateException {
+    public Inventory update(Inventory inventory) {
         Session session = sessionFactory.openSession();
         Transaction transaction = null;
         Inventory newInventory;
+        error.reset();
+
+        if(user == null) {
+            error.setMessage("Unauthorized to update inventory.");
+            return null;
+        }
+
+        if(inventory.getName().isEmpty()) {
+            error.setMessage("Please enter an inventory name.");
+            return null;
+        }
+
+        if(inventory.getQuantity() < 0) {
+            error.setMessage("Quantity must be larger than 0.");
+            return null;
+        }
 
         try {
             transaction = session.beginTransaction();
+
             long id = inventory.getId();
             newInventory = session.get(Inventory.class, id);
 
             if(newInventory != null && newInventory.getUser().getId() == user.getId()) {
+
+                if(newInventory.getName().equals(inventory.getName())) {
+                    error.setMessage("Inventory name must be unique.");
+                    return null;
+                }
+
                 newInventory.setName(inventory.getName());
                 newInventory.setQuantity(inventory.getQuantity());
                 session.update(newInventory);
@@ -91,7 +160,8 @@ public class InventoryTransaction {
                 System.out.println(newInventory.toString());
             }else {
                 System.out.println("Inventory id: " + id + " does not exist");
-                newInventory = null;
+                error.setMessage("Cannot identify inventory.");
+                return null;
             }
 
             transaction.commit();
@@ -100,7 +170,8 @@ public class InventoryTransaction {
                 transaction.rollback();
             }
             e.printStackTrace();
-            throw new HibernateException(e);
+            error.setMessage("Failed to update inventory.");
+            return null;
         }finally {
             session.close();
         }
@@ -108,10 +179,21 @@ public class InventoryTransaction {
         return newInventory;
     }
 
-    public Inventory delete(long id) throws HibernateException {
+    public Inventory delete(long id) {
         Session session = sessionFactory.openSession();
         Transaction transaction = null;
         Inventory inventory;
+        error.reset();
+
+        if(user == null) {
+            error.setMessage("Unauthorized to delete inventory.");
+            return null;
+        }
+
+        if(id < 0) {
+            error.setMessage("Invalid inventory identifier.");
+            return null;
+        }
 
         try {
             transaction = session.beginTransaction();
@@ -124,7 +206,8 @@ public class InventoryTransaction {
                 System.out.println(inventory.toString());
             }else {
                 System.out.println("Inventory id: " + id + " does not exist");
-                inventory = null;
+                error.setMessage("Specified inventory does not exist.");
+                return null;
             }
 
             transaction.commit();
@@ -133,7 +216,8 @@ public class InventoryTransaction {
                 transaction.rollback();
             }
             e.printStackTrace();
-            throw new HibernateException(e);
+            error.setMessage("Failed to delete inventory.");
+            return null;
         }finally {
             session.close();
         }
@@ -141,10 +225,15 @@ public class InventoryTransaction {
         return inventory;
     }
 
-    public List<Inventory> list() throws HibernateException {
+    public List<Inventory> list() {
         Session session = sessionFactory.openSession();
         Transaction transaction = null;
         List<Inventory> list;
+
+        if(user == null) {
+            error.setMessage("Unauthorized to read inventory.");
+            return null;
+        }
 
         try {
             transaction = session.beginTransaction();
@@ -162,11 +251,20 @@ public class InventoryTransaction {
                 transaction.rollback();
             }
             e.printStackTrace();
-            throw new HibernateException(e);
+            error.setMessage("Failed to retrieve inventory data.");
+            return null;
         }finally {
             session.close();
         }
 
         return list;
+    }
+
+    public ErrorHandler getError() {
+        return error;
+    }
+
+    public void setError(ErrorHandler error) {
+        this.error = error;
     }
 }
